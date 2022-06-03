3da3e38bad5e0601eaca9fd302c45d32ef2fd837
StudentMauricioGranados <jgranados00042@ufide.ac.cr>
2022-06-02 20:57:31 -0600

    Añadiendo los primeros pasos del backedn y el RestFul API para el Login y el register

---
+++ Backend/classes/JwtHandler.php
@@ -0,0 +1,56 @@
+<?php
+require './vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JwtHandler
+{
+    protected $jwt_secrect;
+    protected $token;
+    protected $issuedAt;
+    protected $expire;
+    protected $jwt;
+
+    public function __construct()
+    {
+        date_default_timezone_set('America/Costa_Rica');
+        $this->issuedAt = time();
+
+        // TIEMPO DE VIDA DEL TOKEN (3600 second = 1hr)
+        $this->expire = $this->issuedAt + 3600;
+
+        $this->jwt_secrect = "ProyectoSecretSemilla";
+    }
+
+    public function jwtEncodeData($iss, $data)
+    {
+
+        $this->token = array(
+            "iss" => $iss,
+            "aud" => $iss,
+            "iat" => $this->issuedAt,
+            "exp" => $this->expire,
+            // Payload
+            "data" => $data
+        );
+
+        $this->jwt = JWT::encode($this->token, $this->jwt_secrect, 'HS256');
+        return $this->jwt;
+    }
+
+    public function jwtDecodeData($jwt_token)
+    {
+        try {
+            $decode = JWT::decode($jwt_token, $this->jwt_secrect, array('HS256'));
+
+            return [
+                "data" => $decode->data
+            ];
+        } catch (Exception $e) {
+            return [
+                "message" => $e->getMessage()
+            ];
+        }
+    }
+}
