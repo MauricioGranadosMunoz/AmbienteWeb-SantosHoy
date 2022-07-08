6a27fecfb41274cbc689d1ae07f76ecd29595fe7
JoseRamosO <d.anielramos.ov@gmail.com>
2022-07-07 20:32:33 -0600

    Se agrega CREATE de imagen

--- Backend/classes/Noticia.php
+++ Backend/classes/Noticia.php
@@ -1,40 +1,136 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
-    
-    include_once '../../classes/Database.php';
-    include_once '../../classes/Noticia.php';
-    $database = new Database();
-    $db = $database->dbConnection();
-    $items = new Noticia($db);
-    $stmt = $items->getnoticias();
-    $itemCount = $stmt->rowCount();
-
-    echo json_encode($itemCount);
-    if($itemCount > 0){
-        
-        $noticiasArr = array();
-        $noticiasArr["body"] = array();
-        $noticiasArr["itemCount"] = $itemCount;
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            extract($row);
-            $e = array(
-                "id" => $id,
-                "titulo" => $titulo,
-                "descripcion" => $descripcion,
-                "linkasset" => $linkasset,
-                "autor" => $autor,
-                "created" => $created,
-                "tipo" => $tipo;
-            );
-            array_push($noticiasArr["body"], $e);
+    class Noticia{
+        // Connection
+        private $conn;
+        // Table
+        private $db_table = "noticias";
+        // Columns
+        public $id;
+        public $titulo;
+        public $descripcion;
+        public $linkasset;
+        public $autor;
+        public $created;
+        public $tipo;
+        // Db connection
+        public function __construct($db){
+            $this->conn = $db;
         }
-        echo json_encode($noticiasArr);
-    }
-    else{
-        http_response_code(404);
-        echo json_encode(
-            array("message" => "No se encontro la noticia")
-        );
+        // GET ALL
+        public function getnoticias(){
+            $sqlQuery = "SELECT id, titulo, descripcion, linkasset, autor, created, tipo FROM " . $this->db_table . "";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+        // CREATE
+        public function crearNoticia(){
+            $sqlQuery = "INSERT INTO
+                        ". $this->db_table ."
+                    SET
+                        titulo = :titulo, 
+                        descripcion = :descripcion, 
+                        linkasset = :linkasset, 
+                        autor = :autor, 
+                        created = :created,
+                        tipo= :tipo";
+            $stmt = $this->conn->prepare($sqlQuery);
+            // sanitize
+            $this->titulo=htmlspecialchars(strip_tags($this->titulo));
+            $this->descripcion=htmlspecialchars(strip_tags($this->descripcion));
+            $this->linkasset=htmlspecialchars(strip_tags($this->linkasset));
+            $this->autor=htmlspecialchars(strip_tags($this->autor));
+            $this->created=htmlspecialchars(strip_tags($this->created));
+            $this->tipo=htmlspecialchars(strip_tags($this->tipo));
+            // bind data
+            $stmt->bindParam(":titulo", $this->titulo);
+            $stmt->bindParam(":descripcion", $this->descripcion);
+            $stmt->bindParam(":linkasset", $this->linkasset);
+            $stmt->bindParam(":autor", $this->autor);
+            $stmt->bindParam(":created", $this->created);
+            $stmt->bindParam(":tipo", $this->tipo);
+            if($stmt->execute()){
+               return true;
+            }
+            return false;
+        }
+        // READ single
+        public function getSinglenoticia(){
+            $sqlQuery = "SELECT
+                        id, 
+                        titulo, 
+                        descripcion, 
+                        linkasset, 
+                        autor, 
+                        created,
+                        tipo
+                      FROM
+                        ". $this->db_table ."
+                    WHERE 
+                       id = ?
+                    LIMIT 0,1";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->bindParam(1, $this->id);
+            $stmt->execute();
+            $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->titulo = $dataRow['titulo'];
+            $this->descripcion = $dataRow['descripcion'];
+            $this->linkasset = $dataRow['linkasset'];
+            $this->autor = $dataRow['autor'];
+            $this->created = $dataRow['created'];
+            $this->tipo = $dataRow['tipo'];
+        }        
+        // UPDATE
+        public function updatenoticias(){
+            $sqlQuery = "UPDATE
+                        ". $this->db_table ."
+                    SET
+                        titulo = :titulo, 
+                        descripcion = :descripcion, 
+                        linkasset = :linkasset, 
+                        autor = :autor, 
+                        created = :created,
+                        tipo = :tipo
+                    WHERE 
+                        id = :id";
+        
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            $this->titulo=htmlspecialchars(strip_tags($this->titulo));
+            $this->descripcion=htmlspecialchars(strip_tags($this->descripcion));
+            $this->linkasset=htmlspecialchars(strip_tags($this->linkasset));
+            $this->autor=htmlspecialchars(strip_tags($this->autor));
+            $this->created=htmlspecialchars(strip_tags($this->created));
+            $this->id=htmlspecialchars(strip_tags($this->id));
+            $this->tipo=htmlspecialchars(strip_tags($this->tipo));
+            // bind data
+            $stmt->bindParam(":titulo", $this->titulo);
+            $stmt->bindParam(":descripcion", $this->descripcion);
+            $stmt->bindParam(":linkasset", $this->linkasset);
+            $stmt->bindParam(":autor", $this->autor);
+            $stmt->bindParam(":created", $this->created);
+            $stmt->bindParam(":tipo", $this->tipo);
+            $stmt->bindParam(":id", $this->id);
+        
+            if($stmt->execute()){
+               return true;
+            }
+            return false;
+        }
+        // DELETE
+        function deletenoticia(){
+            $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = ?";
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            $stmt->bindParam(1, $this->id);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
     }
 ?>
