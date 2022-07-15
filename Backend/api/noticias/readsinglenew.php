e657953e3ad2bd586f008a81922ee53540f4d254
StudentMauricioGranados <jgranados00042@ufide.ac.cr>
2022-07-14 20:59:51 -0600

    test

---
+++ Backend/api/noticias/readsinglenew.php
@@ -0,0 +1,19 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    require '../../classes/Database.php';
+    require '../../classes/Noticia.php';
+    require '../../AuthMiddleware.php';
+
+    $database = new Database();
+    $db = $database->dbConnection();
+    $item = new Noticia($db);
+    $data = json_decode(file_get_contents("php://input"));
+
+    $item->id = $data->noticia;
+    
+    echo json_encode($item->getSinglenoticia());
+?>
--- javascript/helpers/adminHelper.js
+++ javascript/helpers/adminHelper.js
@@ -52,6 +52,31 @@ $( document ).ready(() => {
         
     }
 
+    const getSingleNoticiasData = async (noticiaId) => {
+        console.log(noticiaId)
+        const requestBody = ` 
+            {
+                "noticia": "${noticiaId}"
+            }
+        `
+        const response = await fetch("http://localhost/ambienteweb-santoshoy/Backend/api/noticias/readsinglenew.php", {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            },
+            body: requestBody,
+        });
+        
+        response.json().then(({titulo,linkasset}) => {
+            
+            $("#input-titulo-editar").val(titulo);
+            $("#input-img-editar").attr("src",`http://localhost${linkasset}`);
+            $( "#input-img-editar" ).addClass( "show" );
+            
+        });
+    }
+
 
     const getNoticiasData = async () => {
     
@@ -67,7 +92,7 @@ $( document ).ready(() => {
             $('#noticias-container').empty();
             noticias.forEach(({id, titulo,autor, tipo,descripcion,linkasset,created}) => {
                 $('#noticias-container').append(`
-                    <div class="card" id="${id}">
+                    <div class="card">
                         <div class="card-image-container">
                             <p class="card-id-section">${id}</p>
                             <img src="http://localhost${linkasset}" class="card-img-top" alt="...">
@@ -93,11 +118,11 @@ $( document ).ready(() => {
                                         <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
                                     </svg>
                                 </a>
-                                <a href="#" class="btn btn-warning">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <button class="btn btn-warning editNoticiasModalButton" id="${id}" data-toggle="modal" data-target="#editNoticiasModal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" id="${id}" viewBox="0 0 16 16">
                                         <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                                     </svg>
-                                </a>
+                                </button>
                                 <a href="#" class="btn btn-danger">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
@@ -121,6 +146,12 @@ $( document ).ready(() => {
     $( "#agregar-noticia-cta" ).click(() => {
         guardarNoticia();
     });
+
+    $('#noticias-container').on('click', '.editNoticiasModalButton', (e) => {
+        getSingleNoticiasData(e.target.id);
+    });
+
+    
 
     $('#noticia-img-upload').on("change", (e) => {
         const output = document.getElementById('output-img');
